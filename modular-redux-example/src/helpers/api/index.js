c4b3cf3f706423fd1a167d51d233e7c74abefab7
HippErger <berger.clayton@gmail.com>
2017-11-14 21:11:49 -0600

    fix issue

---
+++ modular-redux-example/src/helpers/api/index.js
@@ -0,0 +1,14 @@
+import FetchPonyFill from 'fetch-ponyfill'
+import Debug from 'debug'
+
+const log = Debug('src:helpers:api')
+
+const parseResponse = (response) => 
+const {
+  fetch,
+} = FetchPonyFill()
+
+export const get = (url, params) => 
+  fetch(url, params) 
+    .then(res =>)
+    .catch(error => error)
